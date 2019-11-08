e374915a381b2de500ac6eeb0a01236ed92bd6b3
Ciyang <896681584@qq.com>
2019-11-08 17:27:08 +0800

    Zhihu Shield

--- zhpingbi.js
+++ zhpingbi.js
@@ -1,86 +1,84 @@
 // ==UserScript==
-// @name         Zhihu Shield
+// @name         真·知乎屏蔽 Truly Zhihu Shield
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.2.0
 // @description  Shield information you don't want to see.
 // @author       Ciyang
 // @match        https://www.zhihu.com/*
 // @match        http://www.zhihu.com/*
-// @exclude      https://www.zhihu.com/people/*
-// @exclude      http://www.zhihu.com/people/*
 // @grant        none
 // ==/UserScript==
 
 'use strict';
 
 (function () {
-    let mRequest = (medth, href, fn) => {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open(medth, href, true);
-        xmlhttp.onreadystatechange = () => {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                console.log(xmlhttp);
-                fn(xmlhttp.responseText);
-            }
-        };
-        xmlhttp.send();
+  let getContentElement = () => { return document.getElementById('root'); };
+  let isCard = (className) => { return className.search('NestComment') !== -1 || className.search('Card') !== -1 || className.search('List-item') !== -1 || className.search('openComment') !== -1; };
+  let mRequest = (method, href, fn) => {
+    let xmlHttp = new window.XMLHttpRequest();
+    xmlHttp.open(method, href, true);
+    xmlHttp.onreadystatechange = () => { if (xmlHttp.readyState == 4 && xmlHttp.status == 200) { fn(xmlHttp.responseText); } };
+    xmlHttp.send();
+  };
+  let userShieldMap = new Map();
+  let excludeEditor = () => { return window.location.href.search("www.zhihu.com/people/") !== -1 || window.location.href.search("www.zhihu.com/org/") !== -1; };
+  let getUserShielded = (username, href, callback) => {
+    if (!userShieldMap.has(username)) {
+      mRequest('get', href, (resText) => {
+        let res = (resText.search('已屏蔽') !== -1);
+        userShieldMap.set(username, res);
+        callback(res);
+      });
+    } else {
+      callback(userShieldMap.get(username));
     }
-
-    let userShieldMap = new Map();
-    // [string, boolean]
-
-    let getUserShielded = (username, href, callback) => {
-        if (!userShieldMap.has(username)) {
-            mRequest('get', href, (htmltext) => {
-                let res = (htmltext.search('已屏蔽') !== -1);
-                userShieldMap.set(username, res);
-                callback(res);
+  };
+  let checkShielded = () => {
+    let userList = document.getElementsByClassName('UserLink-link');
+    for (const user of userList) {
+      if (user.innerText && user.href) {
+        let card = user;
+        while (card.id !== 'root' && (!card.className || !isCard(card.className))) { card = card.parentElement; }
+        if (card.id !== 'root') {
+          ((userElement, cardElement) => {
+            let url = new URL(userElement.href, window.location.href);
+            getUserShielded(userElement.innerText, url.href, res => {
+              if (res) { cardElement.innerHTML = ['<div>内容作者', userElement.innerText, '已被拉入黑名单</div>'].join(''); }
             });
-        } else {
-            callback(userShieldMap.get(username));
+          })(user, card);
         }
+      }
     }
-
-    let timer;
-
-    let checkShielded = () => {
-        let userList = document.getElementsByClassName('UserLink-link');
-        for (const user of userList) {
-            if (user.innerText && user.href) {
-                (userLinkElement => {
-                    let url = new URL(userLinkElement.href, window.location.href);
-                    getUserShielded(userLinkElement.innerText, url.href, res => {
-                        if (res) {
-                            let addCard = userLinkElement;
-                            while (addCard.className.search('Card') === -1) addCard = addCard.parentElement;
-                            addCard.innerHTML = ["<p>回答者", userLinkElement.innerText, "被拉入黑名单</p>"].join('');
-                            // addCard.remove();
-                        }
-                    });
-                })(user);
-            }
-        }
+  };
+  let timer;
+  let moCallback = (mutationsList, observer) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type == 'childList' && mutation.addedNodes && mutation.addedNodes.length) {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(checkShielded, 50);
+      }
     }
-
-    window.addEventListener('load', (event) => {
-        checkShielded();
-
-        let moCallback = (mutationsList, observer) => {
-            for (const mutation of mutationsList) {
-                if (mutation.type == 'childList' && mutation.addedNodes && mutation.addedNodes.length) {
-                    console.log('A child node has been added');
-                    if (timer) clearTimeout(timer);
-                    timer = setTimeout(checkShielded, 100);
-                }
-            }
-        };
-
-        let targetNode = document.getElementById('TopstoryContent');
-
-        let config = { childList: true, subtree: true };
-
-        let observer = new MutationObserver(moCallback);
-
-        observer.observe(targetNode, config);
-    });
+  };
+  let observerConfig = { childList: true, subtree: true };
+  let openFlag = false;
+  let checkFullPageModal = () => {
+    if (!openFlag && document.getElementsByClassName('Modal--fullPage').length) {
+      openFlag = true;
+      let modal = document.getElementsByClassName('Modal--fullPage')[0];
+      let observer = new window.MutationObserver(moCallback);
+      observer.observe(modal, observerConfig);
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(checkShielded, 50);
+    }
+    if (!document.getElementsByClassName('Modal--fullPage').length) { openFlag = false; }
+  }
+  window.addEventListener('load', (event) => {
+    if (excludeEditor()) { userShieldMap.set(document.getElementsByClassName('ProfileHeader-name')[0].innerText, false); }
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(checkShielded, 50);
+    let targetNode = getContentElement();
+    let observer = new window.MutationObserver(moCallback);
+    observer.observe(targetNode, observerConfig);
+    setInterval(checkFullPageModal, 10);
+  });
 })();
